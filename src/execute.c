45881612efb51add49cca893a4ec1dbf2ca5911e
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-11-20 15:32:43 -0400

    Add ~ for home directory shortcut

--- src/execute.c
+++ src/execute.c
@@ -1,7 +1,7 @@
-#include <stdio.h>   // printf
-#include <stdlib.h>  // strtol
+#include <stdio.h>   // printf, snprintf
+#include <stdlib.h>  // getenv
 #include <unistd.h>  // fork, wait, execvp, chdir, pid_t
-#include <string.h>  // strcmp
+#include <string.h>  // strcmp, strncpy, strcat, strlen
 #include <pthread.h> //
 
 
@@ -25,11 +25,25 @@ void execute(char **argv)
     // Check for built-in functions
     if (strcmp(argv[0], "cd") == 0) 
     {
-        // Change Directory
-        if (chdir(argv[1]) == 0) 
+        // If using shortcut ~ for home, fix up path
+        if (argv[1][0] == '~')
         {
-            return;
-        } 
+            // Get the current users home directory
+            char home[256];
+            snprintf(home, sizeof home, "%s%s%s", "/Users/", getenv("USER"), "/");
+
+            // If deeper than home, make path
+            if (argv[1][1] == '/')
+            {
+                char path[strlen(argv[1]) - 1];
+                strncpy(path, &argv[1][2], strlen(argv[1]) - 1);
+                strcat(home, path);
+            }
+            argv[1] = home;
+        }
+
+        // Change Directory
+        if (chdir(argv[1]) == 0) return;
         else 
         {
             printf("No such file or directory '%s'.\n", argv[1]);

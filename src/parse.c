c4609108f8c00746ad7a14ba54ad3e3fcee40a3a
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-11-22 21:42:50 -0400

    comment parse.

--- src/parse.c
+++ src/parse.c
@@ -2,38 +2,49 @@
 
 void parse(char *line, char **argv)
 {
-    while (*line != '\0')         /* if not the end of line ....... */
+    // While not the end of line
+    while (*line != '\0')
     {
-        // printf("Line before : %s\n", line);
+        printf("Top of while loop\n");
+        
+        // While there are spaces, tabs or new lines replace with null
+        printf("Line before nested while loop 1: %s\n", line);
         while (*line == ' ' || *line == '\t' || *line == '\n') 
         {
-            // printf("Replace white space: %c\n", *line);
-            *line++ = '\0';     /* replace white spaces with 0    */
+            printf("Replace space, tab, or new line: %c\n", *line);
+            *line++ = '\0';     
         }
-        // printf("Line after: %s\n", line);
-        *argv++ = line;          /* save the argument position     */
+        printf("Line after nested while loop 1: %s\n", line);
+        
+        // save the position of argument
+        *argv++ = line;
+
+        // while not null, space, tab, or new line
         while (*line != '\0' && *line != ' ' &&
                 *line != '\t' && *line != '\n') 
         {
+            // If backslash skip a character (allows user to have spaces in filenames)
             if (*line == '\\') 
             {
-                // printf("Skip backslash: %c\n", *line);
-                // printf("Ignoring next character: %c\n", *(line+1));
-                // Rotation String Left and remove the backslash
+                printf("Skip backslash: %c\n", *line);
+                printf("Ignoring next character: %c\n", *(line+1));
+                //Rotation String Left and remove the backslash
                 char *c = line;
                 while (*c != '\0') 
                 {
                     *c = *(c+1);
                     c++;
                 }
-                line++; // Skip the next character
-            } 
+                line++;
+            }
+            // Otherwise skip through until end of argument 
             else 
             {
-                // printf("Skip: %c\n", *line);
-                line++;             /* skip the argument until ...    */
+                printf("Skip: %c\n", *line);
+                line++;
             }
         }
     }
-    *argv = (char *) '\0';                 /* mark the end of argument list  */
+    // Null terminate the argument list
+    *argv = (char *) '\0';
 }

7992632f3f1f51dff198dfc809e1a30b3e428159
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-11-22 22:34:55 -0400

    Set up bitmaps for redirection.

--- src/execute.c
+++ src/execute.c
@@ -22,25 +22,73 @@
 
 void execute(char **argv)
 {
+    // Create bitmaps
+    int pipes[64] = {0};
+    int redirectNew[64] = {0};
+    int redirectAppend[64] = {0};
+    int extremeStart[64] = {0};
+    int extremeEnd[64] = {0};
+    int extremeMiddle[64] = {0};
+    int count = 0;
+    while(1) 
+    {
+        if (argv[count] == '\0' && argv[count+1] == '\0') break;
+        else
+        {
+            if (strcmp(argv[count], ">") == 0) 
+            {
+                redirectNew[count] = 1;
+                printf("> at argv[%d]\n", count);
+            }
+            else if (strcmp(argv[count], ">>") == 0) 
+            {
+                redirectAppend[count] = 1;
+                printf(">> at argv[%d]\n", count);
+            }
+            else if (strcmp(argv[count], "|") == 0) 
+            {
+                pipes[count] = 1;
+                printf(" | at argv[%d]\n", count);
+            }
+            else if (strcmp(argv[count], "<::") == 0) 
+            {
+                extremeStart[count] = 1;
+                printf(" <:: at argv[%d]\n", count);
+            }
+            else if (strcmp(argv[count], "::>") == 0) 
+            {
+                extremeEnd[count] = 1;
+                printf(" ::> at argv[%d]\n", count);
+            }
+            else if (strcmp(argv[count], ":") == 0) 
+            {
+                extremeMiddle[count] = 1;
+                printf(" : at argv[%d]\n", count);
+            }
+            // else if (strcmp(argv[count], "") == 0) {}
+        }
+        count++;
+    }
+
     // Check for built-in functions
     if (strcmp(argv[0], "cd") == 0) 
     {
         // If using shortcut ~ for home, fix up path
-        if (argv[1][0] == '~')
-        {
-            // Get the current users home directory
-            char home[256];
-            snprintf(home, sizeof home, "%s%s%s", "/Users/", getenv("USER"), "/");
+        // if (argv[1][0] == '~')
+        // {
+        //     // Get the current users home directory
+        //     char home[256];
+        //     snprintf(home, sizeof home, "%s%s%s", "/Users/", getenv("USER"), "/");
 
-            // If deeper than home, make path
-            if (argv[1][1] == '/')
-            {
-                char path[strlen(argv[1]) - 1];
-                strncpy(path, &argv[1][2], strlen(argv[1]) - 1);
-                strcat(home, path);
-            }
-            argv[1] = home;
-        }
+        //     // If deeper than home, make path
+        //     if (argv[1][1] == '/')
+        //     {
+        //         char path[strlen(argv[1]) - 1];
+        //         strncpy(path, &argv[1][2], strlen(argv[1]) - 1);
+        //         strcat(home, path);
+        //     }
+        //     argv[1] = home;
+        // }
 
         // Change Directory
         if (chdir(argv[1]) == 0) return;

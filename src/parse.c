9278de436e91f2b1625cdf7460ac588b6d3ed84a
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-11-22 22:34:31 -0400

    comment log info.

--- src/parse.c
+++ src/parse.c
@@ -5,16 +5,16 @@ void parse(char *line, char **argv)
     // While not the end of line
     while (*line != '\0')
     {
-        printf("Top of while loop\n");
+        // printf("Top of while loop\n");
         
         // While there are spaces, tabs or new lines replace with null
-        printf("Line before nested while loop 1: %s\n", line);
+        // printf("Line before nested while loop 1: %s\n", line);
         while (*line == ' ' || *line == '\t' || *line == '\n') 
         {
-            printf("Replace space, tab, or new line: %c\n", *line);
+            // printf("Replace space, tab, or new line: %c\n", *line);
             *line++ = '\0';     
         }
-        printf("Line after nested while loop 1: %s\n", line);
+        // printf("Line after nested while loop 1: %s\n", line);
         
         // save the position of argument
         *argv++ = line;
@@ -26,8 +26,8 @@ void parse(char *line, char **argv)
             // If backslash skip a character (allows user to have spaces in filenames)
             if (*line == '\\') 
             {
-                printf("Skip backslash: %c\n", *line);
-                printf("Ignoring next character: %c\n", *(line+1));
+                // printf("Skip backslash: %c\n", *line);
+                // printf("Ignoring next character: %c\n", *(line+1));
                 //Rotation String Left and remove the backslash
                 char *c = line;
                 while (*c != '\0') 
@@ -40,7 +40,7 @@ void parse(char *line, char **argv)
             // Otherwise skip through until end of argument 
             else 
             {
-                printf("Skip: %c\n", *line);
+                // printf("Skip: %c\n", *line);
                 line++;
             }
         }

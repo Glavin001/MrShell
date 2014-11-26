11973becc573f6afc281cddffc469b594a91f9ba
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-11-26 00:36:39 -0400

    Closes #19. Handle Quoted Arguments Correctly

--- src/parse.c
+++ src/parse.c
@@ -2,6 +2,8 @@
 
 void parse(char *line, char **argv)
 {
+    char beginQuote = '\0';
+
     // While not the end of line
     while (*line != '\0')
     {
@@ -12,7 +14,7 @@ void parse(char *line, char **argv)
         while (*line == ' ' || *line == '\t' || *line == '\n') 
         {
             // printf("Replace space, tab, or new line: %c\n", *line);
-            *line++ = '\0';     
+            *line++ = '\0';
         }
         // printf("Line after nested while loop 1: %s\n", line);
         
@@ -21,14 +23,15 @@ void parse(char *line, char **argv)
 
         // while not null, space, tab, or new line
         while (*line != '\0' && *line != ' ' &&
-                *line != '\t' && *line != '\n') 
+                *line != '\t' && *line != '\n')
         {
             // If backslash skip a character (allows user to have spaces in filenames)
             if (*line == '\\') 
             {
                 // printf("Skip backslash: %c\n", *line);
                 // printf("Ignoring next character: %c\n", *(line+1));
-                //Rotation String Left and remove the backslash
+                
+                // Rotation String Left and remove the backslash
                 char *c = line;
                 while (*c != '\0') 
                 {
@@ -37,8 +40,39 @@ void parse(char *line, char **argv)
                 }
                 line++;
             }
-            // Otherwise skip through until end of argument 
-            else 
+            else if (*line == '"')
+            {
+                // fprintf(stderr, "Begin quote: %s\n", line);
+                beginQuote = *line;
+
+                // Rotation String Left and remove the quotation char
+                char *c = line;
+                while (*c != beginQuote 
+                    || c == line)
+                {
+                    // fprintf(stderr, "before char: %s\n", c);
+                    *c = *(c+1);
+                    // fprintf(stderr, "after char: %s\n", c);
+                    c++;
+                }
+                // fprintf(stderr, "End quote: %s\n", c);
+                c--;
+                line = c;
+                // fprintf(stderr, "End line: %s\n", line);
+
+                // Rotation String Left and remove the quotation char
+                while (*c != '\0')
+                {
+                    // fprintf(stderr, "befor char: %s\n", c);
+                    *c = *(c+2);
+                    // fprintf(stderr, "after char: %s\n", c);
+                    c++;
+                }
+                // fprintf(stderr, "End c: '%s', line: '%s'\n", c, line);
+
+            }
+            // Otherwise skip through until end of argument
+            else
             {
                 // printf("Skip: %c\n", *line);
                 line++;
